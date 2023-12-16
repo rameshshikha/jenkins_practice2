3308d339a4692546e4bec3c02077d7e9d289f16c
rameshshikha <222shikhap@gmail.com>
2023-12-16 10:46:35 +0530

    committed

---
+++ src/test/java/jenkins_mavenproject2/jenkins_TEST.java
@@ -0,0 +1,12 @@
+package jenkins_mavenproject2;
+
+import org.testng.annotations.Test;
+
+@Test
+public class jenkins_TEST {
+
+	public void jenkinsTEST() {
+	System.out.println("testing done successfully");
+	}
+
+}
